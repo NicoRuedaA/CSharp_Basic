1240c4dce170e07dfb75f4c0a63965cb97c78a87
Nico <nico99rueda@gmail.com>
2024-02-09 19:21:02 +0100

    09/02/24

--- C#/Lab 4/Hyperspace Cheese Battle game/Program.cs
+++ C#/Lab 4/Hyperspace Cheese Battle game/Program.cs
@@ -19,6 +19,10 @@ namespace HyperSpaceCheeseGame // Note: actual namespace depends on the project 
 
 
 
+
+        static Vector2 winPos = new Vector2(7, 7);
+
+
         const int TABLE_HIGH = 8, TABLE_LENGTH = 8;
 
         static int[,] _board = new int[,] {
@@ -55,7 +59,6 @@ namespace HyperSpaceCheeseGame // Note: actual namespace depends on the project 
 
         static char readChar(string prompt)
         {
-            char result;
             string stringAux;
             stringAux = readString(prompt);
             char[] characters = stringAux.ToCharArray();
@@ -84,73 +87,15 @@ namespace HyperSpaceCheeseGame // Note: actual namespace depends on the project 
         }
 
 
-        static void PrintTable(Player player)
-        {
 
 
 
-            for (int j = TABLE_HIGH - 1; j >= 0; j--)
-            {
-                for (int i = 0; i < TABLE_LENGTH; i++)
-                {
-                    //    Console.Write($" {boardTable[i, j]}");
-                    if (player.vec == new Vector2(i, j))
-                    {
-                        Console.Write(player.token.PadRight(5));
-                    }
-                    else Console.Write(boardTable[i, j].PadRight(5));
-                }
-                Console.WriteLine();
-            }
-        }
-
-        static int RollDices(Player player)
-        {
-            int min = 1, max = 7, result;
-            Random random = new Random();
-            result = random.Next(min, max);
-            Console.WriteLine($"{player} roll result is " + result);
-            return result;
-        }
-
-        private static void Move(ref Player player, int diceResult)
-        {
-            Console.WriteLine($"Player {player.name} position actual is {player.vec}");
-            Console.WriteLine($"{player.vec} is a cell of type {_board[(int)player.vec.X, (int)player.vec.Y]}");
-
-            int actualPosCellType = _board[(int)player.vec.X, (int)player.vec.Y];
-            Vector2 direction = new Vector2(0, 0);
-            switch (actualPosCellType)
-            {
-                case 1:
-                    direction = new Vector2(0, -1);
-                    player.vec = player.vec + direction * diceResult;
-                    if (player.vec.Y <= 0) player.vec.Y = 0;
-                    break;
-                case 2:
-                    direction = new Vector2(-1, 0);
-                    player.vec = player.vec + direction * diceResult;
-                    if (player.vec.X < 0) player.vec.X = 0;
-                    break;
-                case 3:
-
-                    direction = new Vector2(0, 1);
-                    player.vec = player.vec + direction * diceResult;
-                    if (player.vec.Y >= TABLE_HIGH) player.vec.Y = TABLE_HIGH - 1;
-                    break;
-                case 4:
-                    direction = new Vector2(1, 0);
-                    player.vec = player.vec + direction * diceResult;
-                    if (player.vec.X >= TABLE_LENGTH) player.vec.X = TABLE_LENGTH - 1;
-                    break;
-                default:
-                    Console.WriteLine("ERROR. Invalid Cell tpye");
-                    break;
-            }
 
 
-            Console.WriteLine("New pos is " + player.vec);
-        }
+
+
+
+
 
         static void DecideIfMove(out char _selectionCrit)
         {
@@ -161,91 +106,72 @@ namespace HyperSpaceCheeseGame // Note: actual namespace depends on the project 
         }
 
 
-        static void PreparePlayers(ref int players, ref Player[] names, ref Vector2[] playersPos)
-        {
-            players = readInt("How many players?: ");
-            names = new Player[players];
-            for (int i = 0; i < players; i++)
-            {
-                names[i].name = readString($"Insert player number {i + 1} name: ");
-                names[i].token = names[i].name[0].ToString();
-                names[i].vec = new Vector2(0, 0);
-                playersPos = new Vector2[names.Length];
-            }
-        }
 
 
-        static void PrepareGame(ref int players, ref Player[] names, ref Vector2[] playersPos)
+
+
+
+        static void PrepareGame(ref Player[] players)
         {
-            PreparePlayers(ref players, ref names, ref playersPos);
+            PreparePlayers(ref players);
 
             for (int i = 0; i < TABLE_HIGH; i++)
             {
                 for (int j = 0; j < TABLE_LENGTH; j++)
                 {
-                    switch (_board[i, j])
-                    {
-                        case 1:
-                            boardTable[i, j] = "↓";
-                            break;
-                        case 2:
-                            boardTable[i, j] = "←";
-                            break;
-                        case 3:
-                            boardTable[i, j] = "↑";
-                            break;
-                        case 4:
-                            boardTable[i, j] = "→";
-                            break;
-                        case 0:
-                            boardTable[i, j] = "W";
-                            break;
-                        default:
-                            boardTable[i, j] = "?";
-                            break;
-                    }
+                    boardTable[i, j] = GetCellType(new Vector2(i, j));
                 }
             }
         }
 
-
-        static Vector2 winPos = new Vector2(7, 7);
-
-        static bool CheckIfWin(ref Player player)
+        static void PreparePlayers(ref Player[] players)
         {
-            if (player.vec == winPos)
+            int numPlayers = readInt("How many players?: ");
+            players = new Player[numPlayers];
+
+            for (int i = 0; i < players.Length; i++)
             {
-                Console.WriteLine("Game ENDED");
+                players[i].name = readString($"Insert player number {i + 1} name: ");
+                players[i].token = players[i].name[0].ToString();
+                players[i].vec = new Vector2(0, 0);
             }
-            return player.vec == winPos;
         }
 
-
-        static void PlayerTurn(ref Player player, ref Vector2[] playersPos)
+        static string GetCellType(Vector2 pos)
         {
-            int diceResult = RollDices(player);
-            Move(ref player, diceResult);
-            PrintTable(player);
-        }
-
-        static void DecideIfNewGame(out char c)
-        {
-
-            do
+            string result;
+            switch (_board[(int)pos.X, (int)pos.Y])
             {
-                c = readChar("New game? (N/S)");
-            } while ((c != 'N') && (c != 'S'));
+                case 1:
+                    result = "↓";
+                    break;
+                case 2:
+                    result = "←";
+                    break;
+                case 3:
+                    result = "↑";
+                    break;
+                case 4:
+                    result = "→";
+                    break;
+                case 0:
+                    result = "W";
+                    break;
+                default:
+                    result = "?";
+                    break;
+            }
+            return result;
         }
 
-        private static void PlayRound(Player[] players, ref bool end, ref Vector2[] playersPos)
+
+        private static void PlayRound(Player[] players, ref bool end)
         {
 
 
             for (int i = 0; i < players.Length; i++)
             {
-
-
-                PlayerTurn(ref players[i], ref playersPos);
+                PlayerTurn(ref players, ref players[i]);
                 if (CheckIfWin(ref players[i]))
                 {
                     end = true;
@@ -257,39 +183,199 @@ namespace HyperSpaceCheeseGame // Note: actual namespace depends on the project 
             string aux = readString("Enter any character for continue: ");
         }
 
-        static void Main()
+
+        static void PlayerTurn(ref Player[] players, ref Player actualPlayer)
+        {
+            int diceResult = RollDices(actualPlayer);
+            Move(ref players, ref actualPlayer, diceResult);
+            PrintTable();
+        }
+
+
+        static int RollDices(Player player)
+        {
+            int min = 1, max = 7, result;
+            Random random = new Random();
+            result = random.Next(min, max);
+            Console.WriteLine($"{player.name} roll result is " + result);
+            return result;
+        }
+
+        private static void Move(ref Player[] players, ref Player player, int diceResult)
         {
 
-            //PONER PLAYERS POSITION INICIAL A -1, -1 Y AL JUGAR SI SU POSICION ES -1, -1 HACER QUE SEA IGUAL A 0,0
-            //PARA SIMULAR "PONERLAS EN JUEGO"
+            Vector2 newPos;
 
-            int nextMove, numPlayers = 0;
+            //si no está inicializado iniciamos el juego en 0,0
+            if (player.vec == new Vector2(-1, -1))
+            {
+                player.vec = new Vector2(0, 0);
+            }
+            else boardTable[(int)player.vec.X, (int)player.vec.Y] = GetCellType(player.vec);
 
+            Console.WriteLine($"Player {player.name} position actual is {player.vec}");
+            Console.WriteLine($"{player.vec} is a cell of type {_board[(int)player.vec.X, (int)player.vec.Y]}");
+
+            int actualPosCellType = _board[(int)player.vec.X, (int)player.vec.Y];
+            Vector2 direction = new Vector2(0, 0);
+            switch (actualPosCellType)
+            {
+                case 1:
+                    direction = new Vector2(0, -1);
+                    break;
+                case 2:
+                    direction = new Vector2(-1, 0);
+                    break;
+                case 3:
+                    direction = new Vector2(0, 1);
+                    break;
+                case 4:
+                    direction = new Vector2(1, 0);
+                    break;
+                default:
+                    newPos = new Vector2(0, 0);
+                    Console.WriteLine("ERROR. Invalid Cell tpye");
+                    break;
+            }
+
+            newPos = player.vec + direction * diceResult;
+            if (newPos.Y <= 0) newPos.Y = 0;
+            else if (newPos.X < 0) newPos.X = 0;
+            else if (newPos.Y >= TABLE_HIGH) newPos.Y = TABLE_HIGH - 1;
+            else if (newPos.X >= TABLE_LENGTH) newPos.X = TABLE_LENGTH - 1;
+            player.vec = newPos;
+            CheckIfCellisEmpty(ref players, ref player, ref newPos);
+
+            boardTable[(int)player.vec.X, (int)player.vec.Y] = player.token.ToString();
+            Console.WriteLine("New pos is " + player.vec);
+        }
+
+        static void CheckIfCellisEmpty(ref Player[] players, ref Player player, ref Vector2 cellPos)
+        {
+
+
+            for (int i = 0; i < players.Length; i++)
+            {
+                if (players[i].token == player.token)
+                {
+
+                    continue;
+                }
+
+                else if (players[i].vec == cellPos)
+                {
+
+
+                    Console.WriteLine($"Token of {players[i].name} moved");
+                    PushToken(ref players, ref players[i]);
+                }
+            }
+        }
+
+        static void PushToken(ref Player[] players, ref Player player)
+        {
+
+            Console.WriteLine(player.name);
+            Console.WriteLine($"Position pre push is {player.vec}");
+
+            Vector2 oldPos = player.vec;
+            Vector2 newPos;
+            boardTable[(int)oldPos.X, (int)oldPos.Y] = GetCellType(oldPos);
+
+            //si la ficha esta arriba del todo
+            if (player.vec.Y >= TABLE_HIGH - 1)
+            {
+                //si está en la esquian sup izquierda
+                if (player.vec.X == 0)
+                {
+
+                    newPos = new Vector2(player.vec.X + 1, player.vec.Y);
+
+                    Console.WriteLine("token pushed DOWN");
+
+                }
+                else
+                {
+                    newPos = new Vector2(player.vec.X - 1, player.vec.Y);
+
+                    Console.WriteLine("token pushed DOWN");
+                }
+
+
+            }
+            else
+            {
+
+                newPos = new Vector2(player.vec.X, player.vec.Y + 1);
+                Console.WriteLine($"New position is {newPos}");
+                Console.WriteLine("token pushed UP");
+            }
+
+            player.vec = newPos;
+
+            CheckIfCellisEmpty(ref players, ref player, ref newPos);
+            boardTable[(int)player.vec.X, (int)player.vec.Y] = player.token.ToString();
+            Console.WriteLine($"Position post push is {player.vec}");
+
+
+
+        }
+
+        static bool CheckIfWin(ref Player player)
+        {
+            if (player.vec == winPos)
+            {
+                Console.WriteLine("Game ENDED");
+            }
+            return player.vec == winPos;
+        }
+
+
+
+
+        static void DecideIfNewGame(out char c)
+        {
+
+            do
+            {
+                c = readChar("New game? (N/S)");
+            } while ((c != 'N') && (c != 'S'));
+        }
+
+        static void PrintTable()
+        {
+            for (int j = TABLE_HIGH - 1; j >= 0; j--)
+            {
+                for (int i = 0; i < TABLE_LENGTH; i++)
+                {
+                    Console.Write(boardTable[i, j].PadRight(5));
+                }
+                Console.WriteLine();
+            }
+        }
+
+
+
+        static void Main()
+        {
+            //IMPONER QUE NO PUEDAN HABER 2 TOKENS IGUALES. GENERAR ALEAORIO SI COMIENZAN POR EL MISMO STRIGN HASTA GENERAR 1 VALOR DIFERENTE
+
+            int numPlayers = 0;
             Player[] players = new Player[numPlayers];
-            Vector2[] playerPositions = new Vector2[0];
-            Player actualPlayer;
-            char critKeepMoving, critNewGame;
+            char critNewGame;
             bool gameEnded;
             do
             {
                 gameEnded = false;
-                PrepareGame(ref numPlayers, ref players, ref playerPositions);
+                PrepareGame(ref players);
                 do
                 {
-                    PlayRound(players, ref gameEnded, ref playerPositions);
-
+                    //round = 1turn.player.
+                    //2 players = 1 round of 2 turns
+                    PlayRound(players, ref gameEnded);
                 } while (!gameEnded);
-
                 DecideIfNewGame(out critNewGame);
             } while (critNewGame != 'N');
-
-
-            /*
-            do
-                {
-                   
-                } while (critKeepMoving == 'S');
-                */
         }
     }
 }
